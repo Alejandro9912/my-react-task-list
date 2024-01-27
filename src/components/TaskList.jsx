a1232777a337cae0c900e9df9d273cc6079d2b4f
Alejandro Ríos <juanalcarios@gmail.com>
2024-01-27 17:42:21 -0500

    Add Forms

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,21 +2,59 @@ import Task from "./Task";
 import "../styles/TaskList.css";
 import { useRef } from "react";
 import useTaskManage from "../hooks/useTaskManage";
+import { useForm } from "react-hook-form";
 
 const TaskList = () => {
   const { tasks, createTask, deleteTask, updateTask } = useTaskManage();
-  const inputRef = useRef();
+  const inputNameRef = useRef();
+  const inputDescriptionRef = useRef();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isValid },
+    watch,
+  } = useForm({ mode: "onChange" });
 
   const addTask = () => {
-    createTask(inputRef.current.value);
-    inputRef.current.value = "";
+    const taskName = watch("taskName");
+    const taskDescription = watch("taskDescription");
+    createTask(taskName, taskDescription);
+    inputNameRef.current.value = "";
+    inputDescriptionRef.current.value = "";
   };
 
   return (
     <>
       <div>
-        <input type="text" placeholder="Add Task" ref={inputRef} />
-        <button onClick={addTask}>+</button>
+        <form onSubmit={handleSubmit(addTask)}>
+          <input
+            type="text"
+            placeholder="Add Task"
+            ref={inputNameRef}
+            {...register("taskName", {
+              required: {
+                value: true,
+                message: "Task name is required",
+              },
+              minLength: {
+                value: 3,
+                message: "Min 3 characters",
+              },
+            })}
+          />
+          {errors.taskName && (
+            <span style={{ color: "red" }}>{errors.taskName.message}</span>
+          )}
+          <input
+            type="text"
+            placeholder="Add Description"
+            ref={inputDescriptionRef}
+            {...register("taskDescription")}
+          />
+          <button type="submit" disabled={!isValid}>
+            +
+          </button>
+        </form>
       </div>
       <ul className="list">
         {tasks.map((task) => (
@@ -28,6 +66,7 @@ const TaskList = () => {
           />
         ))}
       </ul>
+      {/* <pre style={{ color: "red" }}>{JSON.stringify(watch(), null, 2)}</pre> */}
     </>
   );
 };
